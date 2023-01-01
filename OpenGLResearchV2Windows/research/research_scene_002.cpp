51db6fe7e6dda39d3d940daba6ab4cea5140db63
Igor Lapin <raynor73@gmail.com>
2023-01-01 03:21:18 +0000

    Add color attribute

--- OpenGLResearchV2Windows/research/research_scene_002.cpp
+++ OpenGLResearchV2Windows/research/research_scene_002.cpp
@@ -10,32 +10,38 @@ static const char* g_vertexShaderSource = R"(
 #version 400 core
 
 layout(location = 0) in vec3 positionAttribute;
+layout(location = 1) in vec3 colorAttribute;
+
+out vec3 interpolatedVertexColor;
 
 void main() {
     gl_Position = vec4(positionAttribute, 1);
+    interpolatedVertexColor = colorAttribute;
 }
 )";
 
 static const char* g_fragmentShaderSource = R"(
 #version 400 core
 
+in vec3 interpolatedVertexColor;
 out vec4 outColor;
 
 void main() {
-    outColor = vec4(1.0, 1.0, 1.0, 1.0);
+    outColor = vec4(interpolatedVertexColor, 1);
 }
 )";
 
 void ResearchScene002::start()
 {
-    GLuint vao;
-    glGenVertexArrays(1, &vao);
-    glBindVertexArray(vao);
-
     GLfloat vertices[] = {
         0.5, -0.5, 0.0,
+        1.0, 0.0, 0.0,
+
         0.0, 0.5, 0.0,
-        -0.5, -0.5, 0.0
+        0.0, 1.0, 0.0,
+
+        -0.5, -0.5, 0.0,
+        0.0, 0.0, 1.0
     };
     GLuint vertexBuffer;
     glGenBuffers(1, &vertexBuffer);
@@ -49,8 +55,15 @@ void ResearchScene002::start()
     auto shaderProgram = m_shadersRepository->getShaderProgramContainer("shaderProgram").shaderProgram();
     glUseProgram(shaderProgram);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0));
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 6, BUFFER_OFFSET(0));
     glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 6, BUFFER_OFFSET(sizeof(GLfloat) * 3));
+    glEnableVertexAttribArray(1);
 
     m_openGLErrorDetector->checkOpenGLErrors("ResearchScene002::start");
 }
